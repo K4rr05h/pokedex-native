111c589be285cdf7da97edd8be7e9238885978ed
K4rr05h <lucenajudymar@gmail.com>
2025-05-18 21:39:40 -0300

    Alterações para usar o context e ajustes na página de detalhes dos pokemons

--- app/[id].tsx
+++ app/[id].tsx
@@ -1,54 +1,55 @@
 import { useEffect, useState } from 'react'
-import { View, Text, ActivityIndicator, Button, Alert } from 'react-native'
+import { View, ActivityIndicator, Alert, TouchableOpacity } from 'react-native'
 import { useLocalSearchParams } from 'expo-router'
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import PokemonDetails from '../src/components/pokemonDetails';
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import PokemonDetails from '../src/components/pokemonDetails'
+import { theme } from '../src/theme/theme'
+import { registerForPushNotificationsAsync } from '../src/notifications/pushNotificationsRegistry';
+import { sendMockNotification } from '../src/notifications/sendMockNotification'
 
 export default function PokemonDetailsPage() {
-    const { id } = useLocalSearchParams()
-    const pokemonId = String(id)
+  const { id } = useLocalSearchParams()
+  const pokemonId = String(id)
 
-    const [liked, setLiked] = useState(false)
-    const [loading, setLoading] = useState(true)
+  const [liked, setLiked] = useState(false)
+  const [loading, setLoading] = useState(true)
 
-    useEffect(() => {
-        async function loadLikeStatus() {
-        try {
-            const stored = await AsyncStorage.getItem(`liked-${pokemonId}`)
-            setLiked(stored === 'true')
-        } catch (err) {
-            console.error('Erro ao carregar like:', err)
-        } finally {
-            setLoading(false)
-        }
-        }
+  useEffect(() => {
+    registerForPushNotificationsAsync();
+  }, []);
 
-        loadLikeStatus()
-    }, [pokemonId])
-
-    async function toggleLike() {
-        try {
-        const newStatus = !liked
-        setLiked(newStatus)
-        await AsyncStorage.setItem(`liked-${pokemonId}`, newStatus.toString())
-        Alert.alert(newStatus ? 'Curtido!' : 'Descurtido!')
-        } catch (err) {
-        console.error('Erro ao salvar like:', err)
-        }
+  useEffect(() => {
+    async function loadLikeStatus() {
+      try {
+        const stored = await AsyncStorage.getItem(`liked-${pokemonId}`)
+        setLiked(stored === 'true')
+      } catch (err) {
+        console.error('Erro ao carregar like:', err)
+      } finally {
+        setLoading(false)
+      }
     }
 
-    if (loading) return <ActivityIndicator size="large" style={{ marginTop: 50 }} />
+    loadLikeStatus()
+  }, [pokemonId])
 
-    return (
-        <View style={{ flex: 1, marginTop: 100 }}>
-            <PokemonDetails nameOrId={pokemonId} />
-            <View style={{ padding: 20 }}>
-                <Button
-                title={liked ? 'Descurtir ❤️' : 'Curtir 🤍'}
-                onPress={toggleLike}
-                color={liked ? 'crimson' : 'gray'}
-                />
-            </View>
-        </View>
-    )
+  async function toggleLike() {
+    try {
+      const newStatus = !liked
+      setLiked(newStatus)
+      await AsyncStorage.setItem(`liked-${pokemonId}`, newStatus.toString())
+      Alert.alert(newStatus ? 'Curtido!' : 'Descurtido!')
+    } catch (err) {
+      console.error('Erro ao salvar like:', err)
+    }
+  }
+
+  if (loading) return <ActivityIndicator size="large" style={{ marginTop: 50 }} />
+
+  return (
+    <View style={{ flex: 1, backgroundColor: theme.colors.card }}>
+      <PokemonDetails nameOrId={pokemonId} />
+      <TouchableOpacity onPress={sendMockNotification}/>
+    </View>
+  )
 }
